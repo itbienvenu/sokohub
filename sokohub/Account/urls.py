9efde992532e99aa3da46981ae740ebcc99db93f
itbienvenu <bienvenugashema@gmail.com>
2025-11-15 17:36:54 +0200

    Finishing the login and register

--- sokohub/Account/urls.py
+++ sokohub/Account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import accounts_list
+from .views import accounts_list, user_login
 
 
 urlpatterns = [
-    path('', accounts_list, name='account-list')
+    path('', accounts_list, name='account-list'),
+    path('login/',user_login, name='user-login' )
 ]
